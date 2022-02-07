0f42608a0badc808a104b607a1b17cfcd7c5b3be
David Keyes <david@rfortherestofus.com>
2022-02-07 12:09:38 -0800

    Work on slides

---
+++ render.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(lubridate)
+library(here)
+library(rmarkdown)
+library(pagedown)
+library(fs)
+library(gmailr)
+library(janitor)
+library(googlesheets4)
+library(googledrive)
+
+
+# Auth --------------------------------------------------------------------
+
+options(gargle_oauth_email = "david@rfortherestofus.com")
+
+# Render report -----------------------------------------------------
+
+render(input = "report.Rmd")
+
+# Render slides -----------------------------------------------------
+
+render(input = "slides.Rmd")
+
+chrome_print("slides.html")
+
+
+# Email -------------------------------------------------------------------
+
+emails <- read_sheet("https://docs.google.com/spreadsheets/d/1TcMCV2BuiBI_svmpFIAk0LTqBXNbXsrxZakYG8dG9dY/edit#gid=1229185329",
+                     sheet = "Responses") %>%
+  # Make the variable names easy to work with
+  clean_names() %>%
+  pull(please_enter_your_email_if_you_want_to_receive_a_copy_of_the_survey_report) %>%
+  unique()
+
+gm_auth_configure()
+
+send_report <- function(email) {
+
+  gm_mime() %>%
+    gm_to(email) %>%
+    gm_from("David Keyes <david@rfortherestofus.com>") %>%
+    gm_subject("Why R is Magic Report") %>%
+    gm_text_body("Please find the report attached.") %>%
+    gm_attach_file("report.html") %>%
+    gm_send_message()
+
+}
+
+emails %>%
+  walk(send_report)
+
+
+
+
